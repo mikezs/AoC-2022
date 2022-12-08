c0778cd98d0cf6cdf286e74ce3d99a4834fb5ad5
Mike Bignell <mike@mbignell.com>
2022-12-08 09:28:43 +0000

    Day 7, the tests pass but doesn't get the solution to my input for part 2

--- Sources/AoCKit/Day7.swift
+++ Sources/AoCKit/Day7.swift
@@ -2,17 +2,119 @@ import Algorithms
 import Foundation
 
 public final class Day7: Day {
-    let input: [Int]
+    class File {
+        let name: String
+        let size: Int
+
+        init(name: String, size: Int) {
+            self.name = name
+            self.size = size
+        }
+    }
+
+    class Folder {
+        let name: String
+        var subfolders: [Folder]
+        var files: [File]
+        let parent: Folder?
+
+        init(name: String, subfolders: [Folder], files: [File], parent: Folder? = nil) {
+            self.name = name
+            self.subfolders = subfolders
+            self.files = files
+            self.parent = parent
+        }
+
+        func subfolder(named: String) -> Folder? {
+            subfolders.first { $0.name == named }
+        }
+
+        var totalSize: Int {
+            subfolders.reduce(0) {
+                $0 + $1.totalSize
+            } + files.reduce(0) { $0 + $1.size }
+        }
+    }
+
+    let input: Folder
 
     public init(input: String) {
-        self.input = input.trimmedLines.compactMap { Int($0) }
+        var folder: Folder?
+        var listing = false
+        var currentFolder: Folder?
+
+        input.trimmedLines.forEach { line in
+            if line.starts(with: "$") {
+                listing = false
+
+                let command = String(line.suffix(from: line.index(line.startIndex, offsetBy: 2)))
+
+                if command.starts(with: "cd") {
+                    let name = String(command.suffix(from: command.index(command.startIndex, offsetBy: 3)))
+
+                    if name == ".." {
+                        currentFolder = currentFolder?.parent
+                    } else {
+                        if folder == nil {
+                            folder = Folder(name: name, subfolders: [], files: [])
+                            currentFolder = folder
+                        } else {
+                            guard let subfolder = currentFolder?.subfolder(named: name) else {
+                                fatalError("I don't have a sub-folder named \(name)")
+                            }
+
+                            currentFolder = subfolder
+                        }
+                    }
+                } else if command.starts(with: "ls") {
+                    listing = true
+                }
+            } else if listing {
+                if line.starts(with: "dir ") {
+                    let name = String(line.suffix(from: line.index(line.startIndex, offsetBy: 4)))
+
+                    currentFolder?.subfolders.append(Folder(name: name, subfolders: [], files: [], parent: currentFolder))
+                } else {
+                    let parts = line.split(separator: " ")
+
+                    currentFolder?.files.append(File(name: String(parts[1]), size: Int(parts[0])!))
+                }
+            }
+        }
+
+        self.input = folder!
+    }
+
+    func values(inside folder: Folder) -> [(folder: Folder, total: Int)] {
+        let subfolders = folder.subfolders.flatMap { values(inside: $0) }
+        let filesTotal = folder.files.reduce(0) { $0 + $1.size }
+        let subfoldersTotal = subfolders.reduce(0) { $0 + $1.total }
+
+        return subfolders + [(folder, subfoldersTotal + filesTotal)]
     }
 
     public func part1() -> Int {
-        return 0
+        values(inside: input)
+            .filter { $0.total < 100000 }
+            .reduce(0) { $0 + $1.total}
     }
 
     public func part2() -> Int {
-        return 0
+        let values = values(inside: input)
+        let total = values.first { $0.folder.name == "/" }.map { $0.total }!
+
+        if total > 70000000 {
+            fatalError("The filesystem is larger than the available space")
+        }
+
+        let required = -(40000000 - total)
+
+        let filtered = values
+            .filter { $0.total > required }
+
+        return filtered.min {
+            $0.total < $1.total
+        }!
+        .total
     }
 }
--- Tests/AoCKitTests/AoC_Tests.swift
+++ Tests/AoCKitTests/AoC_Tests.swift
@@ -147,14 +147,37 @@ mjqjpqmgbljsphdztnvjfqwrcgsmlb
 
     // MARK: - Day 7
     let day7Input = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
 """
 
     func testDay7Part1() {
-        XCTAssertEqual(Day7(input: day7Input).part1(), 0)
+        XCTAssertEqual(Day7(input: day7Input).part1(), 95437)
     }
 
     func testDay7Part2() {
-        XCTAssertEqual(Day7(input: day7Input).part2(), 0)
+        XCTAssertEqual(Day7(input: day7Input).part2(), 24933642)
     }
 
     // MARK: - Day 8
