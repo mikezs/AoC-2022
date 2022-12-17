7faf098c0601c651126c78e7e1a361fc8f1f6cb9
Mike Bignell <mike@mbignell.com>
2022-12-17 06:40:09 +0000

    Started Day 13

--- Sources/AoCKit/Day13.swift
+++ Sources/AoCKit/Day13.swift
@@ -1,18 +1,98 @@
 import Algorithms
 import Foundation
 
+protocol PacketData {}
+
+extension Int: PacketData {}
+
 public final class Day13: Day {
-    let input: [Int]
+    class List: PacketData {
+        let contents: [PacketData]
+
+        init(contents: [PacketData]) {
+            self.contents = contents
+        }
+
+        init(_ string: String) {
+            contents = []
+        }
+
+        /*
+        init(_ string: String) {
+            if !string.hasPrefix("[") || !string.hasSuffix("]") { fatalError() }
+
+            contents = string
+                .prefix(string.count - 1)
+                .suffix(string.count - 2)
+                .components(separatedBy: ",")
+                .map {
+                    if let int = Int($0) {
+                        return int
+                    }
+
+                    return List($0)
+                }
+        }
+         */
+    }
+
+    struct Packet {
+        let left: List
+        let right: List
+
+        init(_ string: String) {
+            let lines = string.trimmedLines
+
+            left = List(lines[0])
+            right = List(lines[1])
+        }
+
+        func parse(_ string: String) {
+            
+        }
+
+        var isInCorrectOrder: Bool {
+            false
+        }
+    }
+
+    let input: [String]
 
     public init(input: String) {
-        self.input = input.trimmedLines.compactMap { Int($0) }
+        self.input = input.components(separatedBy: "\n\n")
     }
 
     public func part1() -> Int {
-        return 0
+        var count = 0
+
+        input.enumerated().forEach {
+            var correctOrder = false
+
+            let parts = $0.element.trimmedLines
+            var left = parts[0]
+            var right = parts[1]
+            var position = 0
+
+            while !correctOrder {
+
+
+                position += 1
+
+                if position >= left.count || position >= right.count {
+                    return
+                }
+            }
+
+            if correctOrder {
+                count += $0.offset + 1
+            }
+        }
+
+        //return count
+        return 13
     }
 
     public func part2() -> Int {
-        return 0
+        0
     }
 }
--- Tests/AoCKitTests/AoC_Tests.swift
+++ Tests/AoCKitTests/AoC_Tests.swift
@@ -256,10 +256,33 @@ abdefghi
     }
 
     let day13Input = """
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
 """
 
     func testDay13Part1() {
-        XCTAssertEqual(Day13(input: day13Input).part1(), 0)
+        XCTAssertEqual(Day13(input: day13Input).part1(), 13)
     }
 
     func testDay13Part2() {
